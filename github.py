a1e46850d1278d24ef2f40c2ebec3d132cc1fce2
John Brandli <john.brandli@yahoo.com>
2023-09-29 14:30:00 -0500

    index code

---
+++ github.py
@@ -0,0 +1,40 @@
+import pickle
+import os
+
+from util import  parse_and_load
+from llama_index.llms import OpenAI
+from llama_index import  GPTVectorStoreIndex, ServiceContext, KeywordTableIndex, StorageContext, load_index_from_storage
+storage_context = None
+index = None
+# print("checking for stored index")
+# if os.path.exists("./storage/index"):
+#     storage_context = StorageContext.from_defaults(persist_dir=f'./storage/index')
+#     index = load_index_from_storage(storage_context=storage_context)
+#     print("use stored index")
+
+# if index is None:
+docs = parse_and_load("repos.yaml")
+storage_context = StorageContext.from_defaults()
+index = GPTVectorStoreIndex.from_documents(docs,storage_context=storage_context)
+print("created new index")
+storage_context.persist(persist_dir=f'./storage/index')
+# llm = OpenAI(temperature=0.1, model="gpt-4")
+# service_context = ServiceContext.from_defaults(llm=llm)
+# index = KeywordTableIndex.from_documents(docs,service_context=service_context)
+print("done creating index")
+chat_engine = index.as_chat_engine()
+
+
+while True:
+    text_input = input("User: ")
+    response = chat_engine.chat(text_input)
+    print(f"Agent: {response}")
+
+# query_engine = index.as_query_engine()
+
+
+# response = query_engine.query("what version of istio is supported with envoy 1.27, reply with a semver only")
+# print(response)
+# assert str(response) == "1.19.x"
+# response = query_engine.query("what versions of istio are supported with kubernetes 1.22, reply with a json array of semver only")
+# print(response)
---
+++ util.py
@@ -0,0 +1,73 @@
+import pickle
+from llama_index.llms import OpenAI
+from llama_index import download_loader, GPTVectorStoreIndex, ServiceContext, KeywordTableIndex
+
+from llama_hub.github_repo import GithubClient, GithubRepositoryReader
+import os
+
+import yaml
+
+def parse_and_load(yaml_file_path):
+    with open(yaml_file_path, 'r') as file:
+        config = yaml.safe_load(file)
+
+    all_docs = []
+    for repo_config in config['repos']:
+        owner = repo_config['owner']
+        repo = repo_config['repo']
+        filters = {
+            "directories": (
+                repo_config['filters']['directories']['value'],
+                getattr(GithubRepositoryReader.FilterType, repo_config['filters']['directories']['type'])
+            ),
+            "file_extensions": (
+                repo_config['filters']['file_extensions']['value'],
+                getattr(GithubRepositoryReader.FilterType, repo_config['filters']['file_extensions']['type'])
+            )
+        }
+        docs = load_repos2(owner, repo, filters)
+        all_docs.extend(docs)
+
+    return all_docs
+
+def load_repos2(owner="istio", repo="istio.io", filters=None):
+    docs = load_docs_if_exist(owner,repo)
+    if docs is None:
+        print(f"repo not cached, loading docs for {repo} from github")
+        download_loader("GithubRepositoryReader")
+        github_client = GithubClient(os.getenv("GITHUB_TOKEN"))
+        
+        # Default filters if none are provided
+        if filters is None:
+            filters = {
+                "directories": (["content/en/docs"], GithubRepositoryReader.FilterType.INCLUDE),
+                "file_extensions": ([".md"], GithubRepositoryReader.FilterType.INCLUDE)
+            }
+    
+        loader = GithubRepositoryReader(
+            github_client,
+            owner=owner,
+            repo=repo,
+            filter_directories=filters["directories"],
+            filter_file_extensions=filters["file_extensions"],
+            verbose=False,
+            concurrent_requests=10,
+        )
+
+        docs = loader.load_data(branch="master")
+        #Save docs to file
+        with open(f"./storage/{owner}_{repo}_docs", 'wb') as file:
+            pickle.dump(docs, file)
+            print(f"saving docs for {repo} to file")
+
+    return docs
+
+def load_docs_if_exist(owner,repo):
+    docs = None
+    if os.path.exists(f"./storage/{owner}_{repo}_docs"):
+        with open(f"./storage/{owner}_{repo}_docs", 'rb') as file:
+            docs = pickle.load(file)
+            print(f"loaded docs for {repo} from cache")
+    return docs
+
+
